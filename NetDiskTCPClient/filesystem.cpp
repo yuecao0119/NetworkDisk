a3a468b24c933a3cbef49a388a8b219696e32792
LightInk2020 <crescent2020@163.com>
2022-07-27 17:16:28 +0800

    add filesystem page design

--- NetDiskTCPClient/filesystem.cpp
+++ NetDiskTCPClient/filesystem.cpp
@@ -2,5 +2,36 @@
 
 FileSystem::FileSystem(QWidget *parent) : QWidget(parent)
 {
+    m_pFileListW = new QListWidget; // 文件列表，显示所有文件
 
+    m_pReturnPrePB = new QPushButton("返回");
+    m_pCreateDirPB = new QPushButton("创建文件夹");
+    m_pDeleteDirPB = new QPushButton("删除文件夹");
+    m_pFlushDirPB = new QPushButton("刷新文件夹");
+
+    QHBoxLayout *pDirOpVBL = new QHBoxLayout;
+    pDirOpVBL -> addWidget(m_pReturnPrePB);
+    pDirOpVBL -> addWidget(m_pCreateDirPB);
+    pDirOpVBL -> addWidget(m_pDeleteDirPB);
+    pDirOpVBL -> addWidget(m_pFlushDirPB);
+
+    m_pRenameFilePB = new QPushButton("重命名文件");
+    m_pUploadFilePB = new QPushButton("上传文件");
+    m_pDownloadFilePB = new QPushButton("下载文件");
+    m_pDeleteFilePB = new QPushButton("删除文件");
+    m_pShareFilePB = new QPushButton("分享文件");
+
+    QHBoxLayout *pFileOpVBL = new QHBoxLayout;
+    pFileOpVBL -> addWidget(m_pRenameFilePB);
+    pFileOpVBL -> addWidget(m_pUploadFilePB);
+    pFileOpVBL -> addWidget(m_pDownloadFilePB);
+    pFileOpVBL -> addWidget(m_pDeleteFilePB);
+    pFileOpVBL -> addWidget(m_pShareFilePB);
+
+    QVBoxLayout *pMainVBL = new QVBoxLayout;
+    pMainVBL -> addLayout(pDirOpVBL);
+    pMainVBL -> addLayout(pFileOpVBL);
+    pMainVBL -> addWidget(m_pFileListW);
+
+    setLayout(pMainVBL);
 }
--- NetDiskTCPClient/filesystem.h
+++ NetDiskTCPClient/filesystem.h
@@ -2,6 +2,10 @@
 #define FILESYSTEM_H
 
 #include <QWidget>
+#include <QListWidget> // 显示文件的列表
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class FileSystem : public QWidget
 {
@@ -10,6 +14,19 @@ public:
     explicit FileSystem(QWidget *parent = nullptr);
 
 signals:
+
+private:
+    QListWidget *m_pFileListW;      // 文件列表，显示所有文件
+
+    QPushButton *m_pReturnPrePB;    // 返回上一路径
+    QPushButton *m_pCreateDirPB;    // 创建文件夹
+    QPushButton *m_pDeleteDirPB;    // 删除文件夹
+    QPushButton *m_pRenameFilePB;   // 重命名文件
+    QPushButton *m_pFlushDirPB;     // 刷新文件夹
+    QPushButton *m_pUploadFilePB;   // 上传文件
+    QPushButton *m_pDownloadFilePB; // 下载文件
+    QPushButton *m_pDeleteFilePB;   // 删除文件
+    QPushButton *m_pShareFilePB;    // 分享文件
 
 };
 
